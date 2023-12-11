56b92cce55914b06db14da7a8e6a8bef4c728572
levyathan <sritharkalimuthu@gmail.com>
2023-12-11 13:59:07 +0530

    Learning Day 11

--- Exercise/Calculator/script.js
+++ Exercise/Calculator/script.js
@@ -1,14 +1,20 @@
+// Storing Calculation to local storage.
 let calculation = localStorage.getItem('calculation') || ``;
 display();
 
+// Function to update button inputs.
 function updateCalculation(value) {
     calculation += value;
     display();
 }
+
+// Function to display in calculation in input box.
 function display() {
     const monitor = document.querySelector('.monitor')
     monitor.value = `${calculation}`
 }
+
+// Function for clear button to remove the stored values from local storage.
 function Clear() {
     calculation = '';
     localStorage.removeItem('calculation');
