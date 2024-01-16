82a88a2ddfb6e9aa5a3b70b9ec544fab29f91d14
levyathan <sritharkalimuthu@gmail.com>
2024-01-16 22:36:02 +0530

    Learning Day 26

--- Weather_Application/index.js
+++ Weather_Application/index.js
@@ -11,6 +11,7 @@ const humidity = document.getElementById('humidity');
 const wind_speed = document.getElementById('wind-speed');
 let weather = document.querySelector('.js-weather');
 let response;
+let data;
 let error_message_timer;
 
 // Adding keyboard Event to Function Call.
@@ -28,17 +29,71 @@ searchButton.addEventListener('click', weathercall)
 function weathercall() {
 
     if (city.value !== ``) {
+        clearscreen();
         weather.classList.remove('js-weather');
         const location = city.value;
         getWeatherData(location);
     }
 }
 
+// Asynchronous Function to fetch data from Openweather with error Handling.
+async function getWeatherData(location) {
+    try {
+        description.classList.add('loader');
+        response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${Apikey}&units=metric`)
+
+
+        data = await response.json();
+        if (response) {
+            description.classList.remove('loader');
+            const temp = Math.round(data.main.temp);
+            const feels_like = data.main.feels_like;
+            const weather_icon = data.weather[0].icon;
+
+            icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${weather_icon}.png" id='icon' alt="Weather_Icon"/>`;
+            temperature.innerText = `${temp}°C`;
+            description.innerText = `${data.weather[0].description}`;
+            city_location.innerHTML = `<ion-icon name="location" id="location-icon"></ion-icon>${data.name}`;
+
+            FeelsLike.innerHTML = `<div class="cols">
+            <p id="FeelsLike">${feels_like}°C</p>
+            <ion-icon name="cloud-circle" id="updates-icon"></ion-icon>
+            <p>Feels Like</p>
+            </div>`;
+            humidity.innerHTML = `<div class="cols">
+            <p id="humidity">${data.main.humidity}%</p>
+            <ion-icon name="water" id="updates-icon"></ion-icon>
+            <p>Humidity</p>
+            </div>`;
+            wind_speed.innerHTML = ` <div class="cols">
+            <p id="wind-speed">${data.wind.speed}m/s</p>
+            <ion-icon name="speedometer" id="updates-icon"></ion-icon>
+            <p>Wind-Speed</p>
+            </div>`;
+        }
+
+    } catch (error) {
+        if (!response) {
+            let error_message = `Check Your Internet Connection!`
+            ErrorDisplay(error_message);
+        } else if (data.cod == 401) {
+            let error_message = `Server Couldn't be Reached!`
+            ErrorDisplay(error_message);
+        } else {
+            let error_message = `${data.message}`
+            ErrorDisplay(error_message);
+        }
+    } finally {
+        city.value = ``;
+    }
+};
+
+
 // Function to display errors.
-function ErrorDisplay(error_content) {
+function ErrorDisplay(content) {
     icon.innerHTML = ``;
     temperature.innerText = ``;
-    description.innerHTML = `${error_content}`;
+    description.innerHTML = `<div id="error">${content}</div>`;
     city_location.innerHTML = ``;
 
     FeelsLike.innerHTML = ``;
@@ -50,53 +105,12 @@ function ErrorDisplay(error_content) {
     }, 6000);
 }
 
-// Asynchronous Function to fetch data from Openweather with error Handling.
-async function getWeatherData(location) {
-    try {
-        response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${Apikey}&units=metric`);
-
-        const data = await response.json();
-
-        const temp = Math.round(data.main.temp);
-        const feels_like = data.main.feels_like;
-        const weather_icon = data.weather[0].icon;
-
-        icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${weather_icon}.png" id='icon' alt="Weather_Icon" />`;
-        temperature.innerText = `${temp}°C`;
-        description.innerText = `${data.weather[0].description}`;
-        city_location.innerHTML = `<ion-icon name="location" id="location-icon"></ion-icon>${data.name}`;
-
-        FeelsLike.innerHTML = `<div class="cols">
-        <p id="FeelsLike">${feels_like}°C</p>
-        <ion-icon name="cloud-circle" id="updates-icon"></ion-icon>
-        <p>Feels Like</p>
-        </div>`;
-        humidity.innerHTML = `<div class="cols">
-        <p id="humidity">${data.main.humidity}%</p>
-        <ion-icon name="water" id="updates-icon"></ion-icon>
-        <p>Humidity</p>
-        </div>`;
-        wind_speed.innerHTML = ` <div class="cols">
-        <p id="wind-speed">${data.wind.speed}m/s</p>
-        <ion-icon name="speedometer" id="updates-icon"></ion-icon>
-        <p>Wind-Speed</p>
-        </div>`;
-
-        city.value = '';
-    } catch (error) {
-        if (!response) {
-            let error_content1 = `<p id="js-error">Check Your Internet Connection!</p>`
-            ErrorDisplay(error_content1);
-            city.value = '';
-        } else if (response.status == 401) {
-            let error_content2 = `<div id="js-server-error"><p id="error-message">Server couldn't be reached!</p></div>`;
-            ErrorDisplay(error_content2);
-            city.value = '';
-        } else if (response.status == 404) {
-            let error_content3 = `<div id="js-server-error"><p id="js-error">Please Check the Spell and Try Again!</p>
-            </div>`
-            ErrorDisplay(error_content3);
-            city.value = '';
-        }
-    }
-};
+function clearscreen() {
+    icon.innerHTML = ``;
+    temperature.innerText = ``;
+    description.innerHTML = ``;
+    city_location.innerHTML = ``;
+    FeelsLike.innerHTML = ``;
+    humidity.innerHTML = ``;
+    wind_speed.innerHTML = ``;
+}
